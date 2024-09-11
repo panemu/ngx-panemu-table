67df14af7bbb3a0d33d21b0c8d2621851ddb5d66
Amrullah <amrullah@panemu.com>
2024-09-12 02:27:05 +0700

    unit test for pagination in RowGroup

--- projects/demo/src/app/example/all-features-client.component.spec.ts
+++ projects/demo/src/app/example/all-features-client.component.spec.ts
@@ -2,6 +2,7 @@ import { provideHttpClient } from '@angular/common/http';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { AllFeaturesClientComponent } from './all-features-client.component';
 import { DebugElement } from '@angular/core';
+import { PanemuTableController } from 'ngx-panemu-table';
 
 //ng test demo --include="projects/demo/src/app/example/all-features-client.component.spec.ts"
 
@@ -57,5 +58,38 @@ describe('AllFeaturesClientComponent', () => {
     //displayed data should be 100
     expect(component.controller.getAllData().length).toBe(100);
     expect(componentEl.querySelectorAll('table tbody tr').length).toBe(100);
+	})
+
+  it('pagination on row group should works', async () => {
+    const oriMaxRows = PanemuTableController.DEFAULT_MAX_ROWS;
+    const paginationSize = 5;
+    PanemuTableController.DEFAULT_MAX_ROWS = paginationSize;
+    component.controller.criteria = [];
+    component.controller.reloadData();
+		await fixture.whenStable();
+    fixture.detectChanges();
+
+		//======== Check initial grouping and filtering
+		const de: DebugElement = fixture.debugElement;
+    const componentEl: HTMLElement = de.nativeElement;
+    let firstCell = componentEl.querySelector('tbody tr td');
+    let groupLabelEl = firstCell?.querySelector('.group-label') as HTMLElement;
+    const philippinesCount = 13;
+    expect(groupLabelEl?.textContent).toBe(`Philippines (${philippinesCount})`);
+    groupLabelEl.click();
+    fixture.detectChanges();
+    await fixture.whenStable();
+
+    let paginationEl = firstCell?.querySelector('panemu-pagination');
+    expect(paginationEl).toBeDefined();
+    
+    const expandedRowsEl = componentEl.querySelectorAll('tbody tr.row');
+    expect(expandedRowsEl.length).toBe(paginationSize);
+    fixture.detectChanges();
+    await fixture.whenStable();
+    expect(paginationEl!.querySelector('span')?.textContent?.trim()).toBe(`/ ${philippinesCount}`);
+    const inputEl = paginationEl!.querySelector('input') as HTMLInputElement;
+    expect(inputEl.value).toBe('1-5');
+    PanemuTableController.DEFAULT_MAX_ROWS = oriMaxRows;
 	})
 })
