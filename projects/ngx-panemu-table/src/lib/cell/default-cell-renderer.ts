18287ebaa04c1337e05a435ffd45dea50066488f
abdun_jos <nafichasani@gmail.com>
2025-05-06 18:12:59 +0700

    Simplify default cell renderer template

--- projects/ngx-panemu-table/src/lib/cell/default-cell-renderer.ts
+++ projects/ngx-panemu-table/src/lib/cell/default-cell-renderer.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input, OnInit, Signal, TemplateRef } from '@angular/core';
+import { Component, OnInit, Signal, TemplateRef, viewChild } from '@angular/core';
 import { PropertyColumn } from '../column/column';
 import { CellComponent, CellRenderer } from './cell';
 import { CellFormatterPipe } from './cell-formatter.pipe';
@@ -13,16 +13,16 @@ export class DefaultCellRenderer implements CellComponent<any>, OnInit {
   row!: any;
   column!: PropertyColumn<any>
   parameter?: any;
-  templateRef?: Signal<TemplateRef<any>>;
-  
+  templateRef = viewChild.required<TemplateRef<any>>('defaultDescriptionTemplate');
+
   ngOnInit(): void {
-    this.templateRef = this.parameter?.templateRef
+    this.templateRef = this.parameter?.templateRef ?? this.templateRef
   }
-  
+
   /**
    * Create a custom cell renderer using `ng-template`.
-   * @param templateRef 
-   * @returns 
+   * @param templateRef
+   * @returns
    */
   static create(templateRef?: Signal<TemplateRef<any> | undefined>): CellRenderer {
     return {
