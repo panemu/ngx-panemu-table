67eed944b5fdc041ec6c1382a0c0ff88e1b560e7
Amrullah <amrullah@panemu.com>
2026-05-18 14:22:25 +0700

    fix column search demo

--- projects/demo/src/app/example/custom-component/column-search-datasource.ts
+++ projects/demo/src/app/example/custom-component/column-search-datasource.ts
@@ -1,4 +1,4 @@
-import { CellFormatter, ColumnDefinition, PanemuTableDataSource, Predicate } from "ngx-panemu-table";
+import { AndPredicate, CellFormatter, ColumnDefinition, PanemuTableDataSource, Predicate } from "ngx-panemu-table";
 
 export class ColumnSearchDataSource<T> extends PanemuTableDataSource<T> {
 
@@ -14,14 +14,15 @@ export class ColumnSearchDataSource<T> extends PanemuTableDataSource<T> {
       return result;
     }
     return result.filter((item: any) => {
-      // for (const crit of tableCriteria) {
-      //   let searchKeyword = String(crit.value).toLowerCase();
-      //   let formattedValue = String((this.formatters[crit.field](item[crit.field]) ?? '')).toLowerCase();
-      //   if (!formattedValue.includes(searchKeyword)) {
-      //     return false;
-      //   }
+      for (const crit of (tableCriteria as AndPredicate).operands) {
+        let aFilter = crit as any;
+        let searchKeyword = String(aFilter.value).toLowerCase();
+        let formattedValue = String((this.formatters[aFilter.field](item[aFilter.field]) ?? '')).toLowerCase();
+        if (!formattedValue.includes(searchKeyword)) {
+          return false;
+        }
 
-      // }
+      }
       return true;
     })
   }
--- projects/demo/src/app/example/custom-component/header-filter.component.ts
+++ projects/demo/src/app/example/custom-component/header-filter.component.ts
@@ -1,7 +1,7 @@
 
 import { Component, OnInit } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
-import { HeaderComponent, HeaderRenderer, PanemuTableController, PropertyColumn } from 'ngx-panemu-table';
+import { AndPredicate, HeaderComponent, HeaderRenderer, PanemuTableController, PropertyColumn } from 'ngx-panemu-table';
 import { debounceTime, distinctUntilChanged } from 'rxjs';
 
 @Component({
@@ -14,26 +14,31 @@ export class HeaderFilterComponent implements OnInit, HeaderComponent {
   controller!: PanemuTableController<any>;
   txtFilter = new FormControl();
   ngOnInit() {
-    // this.controller = this.parameter.controller;
-    // this.txtFilter.valueChanges.pipe(
-    //   debounceTime(250),
-    //   distinctUntilChanged()
-    // ).subscribe(val => {
-    //   let crit = this.controller.criteria.find(item => item.field === this.column.field);
-    //   if (val) {
-    //     if (!crit) {
-    //       crit = {field: String(this.column.field), value: val}
-    //       this.controller.criteria.push(crit)
-    //     } else {
-    //       crit.value = val
-    //     }
-    //   } else {
-    //     if (crit) {
-    //       this.controller.criteria = this.controller.criteria.filter(item => item.field != this.column.field)
-    //     }
-    //   }
-    //   this.controller.reloadData();
-    // })
+    this.controller = this.parameter.controller;
+    this.txtFilter.valueChanges.pipe(
+      debounceTime(250),
+      distinctUntilChanged()
+    ).subscribe(val => {
+      let criteria: AndPredicate = this.controller.criteria as AndPredicate;
+      if (!criteria) {
+        criteria = {type: 'and', operands: []}
+        this.controller.criteria = criteria;
+      }
+      let operand = criteria.operands.find((item: any) => item['field'] === this.column.field);
+      if (val) {
+
+        if (!operand) {
+          criteria.operands.push({type: 'eq', field: String(this.column.field), value: val})
+        } else {
+          (operand as any).value = val;
+        }
+      } else {
+        if (operand) {
+          criteria.operands = criteria.operands.filter(item => item !== operand);
+        }
+      }
+      this.controller.reloadData();
+    })
   }
 
   static create(controller: PanemuTableController<any>): HeaderRenderer {
